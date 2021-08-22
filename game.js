e5c3d38bfaf450732bf0a6dc02cba0ee108e6336
Trang Van <87851721+vantrangaof@users.noreply.github.com>
2021-08-22 23:04:03 +0800

    Update game.js

--- game.js
+++ game.js
@@ -308,7 +308,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html')
+        return window.location.assign('end.html')
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
